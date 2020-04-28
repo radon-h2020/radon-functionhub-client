eee0f68d0ad945b24c607a0edcc83af97e88e505
naesheim <hg.nesheim@gmail.com>
2020-04-28 09:46:27 +0200

    pass token in json

--- cli.py
+++ cli.py
@@ -56,6 +56,7 @@ def deploy_function(global_config, zip_file):
         payload['organization'] = config.get('REPOSITORY','org')
         payload['provider'] = config.get('RUNTIME','provider')
         payload['runtime'] = config.get('RUNTIME','runtime')
+        payload['token'] = global_config.token
         with open(zip_file,'rb') as binfile:
             encoded = base64.b64encode(binfile.read())
         payload['file'] = encoded.decode()
