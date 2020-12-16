5107ef896c5cc4d93d163ff80797a5ff85a676a6
Hans Georg Næsheim <hg.nesheim@gmail.com>
2020-12-16 15:40:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='functionhub',
-    version='0.2.1',
+    version='0.2.2',
     author="naesheim",
     description="A client for CloudStash.io interaction",
     long_description=long_description,
