3810ffdf5b481633a54eb2af226ffce4468420f7
naesheim <hg.nesheim@gmail.com>
2021-04-12 13:32:47 +0200

    added test case and increased coverage

--- test/test_main.py
+++ test/test_main.py
@@ -1,8 +1,6 @@
-from typing import Match
 from functionhub.main import fuhub
 from click.testing import CliRunner
 from click.exceptions import ClickException
-import pytest
 import os
 import shutil
 
@@ -43,6 +41,7 @@ def test_upload_package():
     assert result.output == f"upload function hello-world to repository my-public-functions\nauthentication error\n"
     #CLEANUP
     os.remove(f"{test_folder}.zip")
+    
 
 def test_upload_nonexisting_package():
     runner = CliRunner()
@@ -57,3 +56,18 @@ def test_upload_package_with_missing_config_file():
     assert result.output == "Error: Config file could not be found\n"
     #CLEANUP
     os.remove(f"{test_folder}.zip")
+
+def test_upload_package_wrong_config():
+    missing_option = 'runtime'
+    runner = CliRunner()
+    runner.invoke(fuhub, ['package',test_folder])
+    with open('example/config.ini','r') as org_conf:
+        with open('example/config2.ini','w') as new_conf:
+            for line in org_conf.readlines():
+                if missing_option not in line:
+                    new_conf.write(line)
+    result = runner.invoke(fuhub, ['-f', 'example/config2.ini', 'upload',f"{test_folder}.zip"])
+
+    assert result.output == f"No option '{missing_option}' in section: 'RUNTIME'\n"
+    #CLEANUP
+    os.remove("example/config2.ini")
